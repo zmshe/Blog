ce65571992cdb6981eac1c7572e42c6555a0c433
xijianhao <xijianhao@qianxin.com>
2021-06-10 19:24:47 +0800

    fix:修复login进去首页背景色闪烁

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,26 @@ import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { routerConfig } from "@src/config/router-config";
+import { observer } from "mobx-react";
+import AppStore from "@src/stores/app";
 import "antd/dist/antd.css";
 import "./style/index.css";
 
-const LoadableApp = () => {
+const LoadableApp = observer(() => {
   const currentUser = localStorage.getItem("currentUser");
-
+  const { theme } = AppStore;
   useEffect(() => {
     if (location.pathname === "/" && !currentUser) {
       location.pathname = "/login";
     }
+  }, []);
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme");
+    const rootRef = document.getElementById("root");
+    (rootRef as HTMLElement).style.background = !localTheme
+      ? theme.colour.background
+      : JSON.parse(localTheme).colour.background;
   }, []);
 
   return (
@@ -28,6 +38,6 @@ const LoadableApp = () => {
       </Switch>
     </BrowserRouter>
   );
-};
+});
 
 ReactDOM.render(<LoadableApp />, document.getElementById("root"));
